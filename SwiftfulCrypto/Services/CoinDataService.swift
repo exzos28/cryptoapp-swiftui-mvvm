686a4dcaefb901eac3b791c6e995c46f4ef4122e
exzos <oleksandr.kurinnyi.dev@gmail.com>
2022-05-02 17:59:19 +0200

    Feature: Downloading Coin Images using Combine, MVVM, and a Networking Layer

--- SwiftfulCrypto/Services/CoinDataService.swift
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -18,7 +18,6 @@ class CoinDataService {
     }
     
     private func getCoins() {
-        
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=3") else { return }
         
         coinSubscription = NetworkingManager.download(url: url)
@@ -27,7 +26,6 @@ class CoinDataService {
                 self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
             })
-        
     }
     
     
